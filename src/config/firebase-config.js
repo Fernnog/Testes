cd7ea28e31d89c2123265c2d1143d1363793c606
Fernnog <fernandocnogueira@gmail.com>
2025-06-28 17:16:34 -0400

    Update firebase-config.js

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,7 +1,16 @@
 // src/config/firebase-config.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
+
+
+const firebaseConfig = {
+    // SUAS CREDENCIAIS AQUI
+};
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 const firebaseConfig = { /* ... */ };
 const app = initializeApp(firebaseConfig);
